57c1d07c34c6b6db8c87a5a8889252cbfd5e4f40
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2023-08-22 09:54:28 +1000

    Added functions for add,subtract,multiply and divide

--- calculatorLogic.js
+++ calculatorLogic.js
@@ -1,1 +1,47 @@
-console.log("This works");
+// Function to: Add Numbers
+let number1=0;
+let number2=0;
+let operator;
+let result;
+
+function addNumbers(number1,number2)
+{
+result=number1+number2;
+console.log(result);
+
+}
+
+addNumbers(10,22);
+
+// Function to: subtract Numbers
+
+function subtract(number1,number2)
+{
+result=number1-number2;
+console.log(result);
+
+}
+
+subtract(120,3);
+
+// Function to: multiply Numbers
+
+function multiply(number1,number2)
+{
+
+    result=number1*number2;
+    console.log(result);
+}
+
+multiply(10,2);
+
+// Function to: divide Numbers
+
+function divide(number1,number2)
+{
+    result=number1/number2;
+    console.log(result);
+}
+
+divide(10,2);
+
