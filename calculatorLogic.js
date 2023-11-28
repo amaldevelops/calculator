52162383c1214b749dccc308d2b26f73ec549051
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2023-11-29 10:06:55 +1100

    Create functions to reset number and array values when required

--- calculatorLogic.js
+++ calculatorLogic.js
@@ -25,15 +25,28 @@ let result;
 const calculationArray=[]; // This array is used to capture the numbers and operator from the user input buttons.
 let a=1;
 
+// Following function will clear the calculationArray
+function calculationArrayClear ()
+    {
+        calculationArray.length=0;
+    }
 
+// Following function will set the number 1 and number 2 values to Null
+
+function numberNullSet ()
+    {
+        number1=null;
+        number2=null;
+    }
+
+// Following function will update the calculator display
 function displayUpdate (displayValue)
       {
         const calculatorDisplay=document.querySelector('#displayOutput');
         
 
         calculatorDisplay.textContent=displayValue; // This code will show array in displayout
-           
-    
+               
       }
 
 // *User enters the first number: User can pause and keep continuing to enter the number, this process only stops
@@ -86,11 +99,10 @@ function buttonCapture(){
                         number1=calculationArray.join("") // This will join the array together to a single string
                         number1=parseInt(number1,10); // This will convert string to a number
                         console.log("Number 1 Details: "+ number1+ "Type is" + typeof(number1));
-                        console.log(calculationArray.length=0);
+                        console.log(calculationArrayClear());
                         displayUpdate(number1);
 
-
-                        
+                       
                     }
 
                     else if (number2===null)
@@ -98,7 +110,7 @@ function buttonCapture(){
                         number2=calculationArray.join("") // This will join the array together to a single string
                         number2=parseInt(number2,10); // This will convert string to a number
                         console.log("Number 2 Details: "+ number2+ "Type is" + typeof(number2));
-                        calculationArray.length=0;
+                        console.log(calculationArrayClear());
                         displayUpdate(number2);
                     }
 
@@ -135,8 +147,6 @@ function buttonCapture(){
         
       });
     
-    
-      
     
     });
     }
@@ -182,7 +192,7 @@ let addNumbers=(number1,number2)=>
     result=number1+number2;
     console.log(result);
     displayUpdate(result);
-
+    numberNullSet();
 
 
 }
